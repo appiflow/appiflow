e46d16c6e588cd8af67f448d75e988760710236c
ragscoder <tech4raghu@gmail.com>
2023-11-12 19:25:06 +0530

    Added Rule DTO with json conversion

--- appiflow-backend/src/appiflow/dto/rule_dto.py
+++ appiflow-backend/src/appiflow/dto/rule_dto.py
@@ -1,5 +1,35 @@
+import json
+from json import JSONEncoder
+from types import SimpleNamespace as Namespace
+
 class Rule:
-    def __init__(self) -> None:
-        pass
+    """DTO Class to represent a Rule.  
+    """
+    def __init__(self, name, source_location, target_location, target_name) -> None:
+        self.name, self.source_location, self.target_location, self.target_name = name, source_location, target_location, target_name
+    
+    
+    def from_json(self, rule_json : str) -> 'Rule':
+        """Convert from json to object
+
+        Args:
+            rule_json (str): Rule as json
+
+        Returns:
+            Rule: Object representation of json
+        """
+        return json.loads(rule_json, object_hook = lambda d: Namespace(**d))
+    
+    def to_json(self) -> str:
+        """Convert from object to json
+
+        Returns:
+            str: Json representation of object
+        """
+        return json.dumps(self, indent=4, cls = RuleEncoder)
+        
+    
+class RuleEncoder(JSONEncoder):
+        def default(self, o): return o.__dict__
     
     
--- appiflow-backend/src/appiflow/main.py
+++ appiflow-backend/src/appiflow/main.py
@@ -3,10 +3,18 @@ from template import create_file_rule_handler as crh
 from dto import rule_dto
 
 def main():
-    rule = rule_dto.Rule
+    rule = rule_dto.Rule('a', 'b', 'c', 'd')
     create_file_handler = crh.CreateFileRuleHandler()
     create_file_handler.handle(rule)
     
+
+def test_json_objects():
+    rule = rule_dto.Rule('a', 'b', 'c', 'd')
+    print("rule json: ")
+    a = rule.to_json()
+    print(a)
+    r = rule.from_json(a)
+    print(f"rule from json",{r.name}) 
     
 if __name__ == "__main__":
     main()
